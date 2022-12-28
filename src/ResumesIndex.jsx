982815fa76b411f2c21d6d9130d0a1d953884b00
Cole Price <colevart@gmail.com>
2022-12-28 15:20:19 -0500

    card

--- src/ResumesIndex.jsx
+++ src/ResumesIndex.jsx
@@ -1,14 +1,33 @@
 export function ResumesIndex(props) {
   return (
-    <div>
-      <h1>All Resumes</h1>
-      {props.resumes.map((resume) => (
-        <div key={resume.id}>
-          <h2>{resume.first_name}</h2>
-          <h2>{resume.last_name}</h2>
-          <img src={resume.image_url} />
-        </div>
-      ))}
+    <div id="resume-index">
+      <h1 className="text-center">Resumes!!</h1>
+      <br />
+      <div className="posts row text-bg-success mb-3">
+        {props.resumes.map((resume) => (
+          <div className="col-4" key={resume.id}>
+            <br />
+            <h2 className="card-title text-center">
+              {resume.first_name}
+              {resume.last_name}
+            </h2>
+            <div>
+              <img src={resume.image_url} className="card-img-top rounded mx-auto d-block" alt="" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+
+// <div>
+//   <h1>All Resumes</h1>
+//   {props.resumes.map((resume) => (
+//     <div key={resume.id}>
+//       <h2>{resume.first_name}</h2>
+//       <h2>{resume.last_name}</h2>
+//       <img src={resume.image_url} />
+//     </div>
+//   ))}
+// </div>
