8627eb8858e530375e8422a087f422d67dbcddde
Cole Price <colevart@gmail.com>
2022-12-29 13:44:56 -0500

    float

--- src/ResumesShow.jsx
+++ src/ResumesShow.jsx
@@ -2,17 +2,20 @@ export function ResumesShow(props) {
   return (
     <div>
       <h1>Resume</h1>
-      <p>First_name: {props.resume.First_name}</p>
-      <p>Last_name: {props.resume.Last_name}</p>
-      <p>Email: {props.resume.Email}</p>
-      <p>Phone_number: {props.resume.Phone_number}</p>
-      <p>Short_bio: {props.resume.Short_bio}</p>
-      <p>Linkedin_url: {props.resume.Linkedin_url}</p>
-      <p>Twitter_handle: {props.resume.Twitter_handle}</p>
-      <p>Personal_website_url: {props.resume.Personal_website_url}</p>
-      <p>Online_resume_url: {props.resume.Online_resume_url}</p>
-      <p>Github_url: {props.resume.Github_url}</p>
-      <p>Photo_url: {props.resume.Photo_url}</p>
+      <div class="float-end">
+        <img src={props.resume.photo_url} />
+      </div>
+      <br></br>
+      <p>First_name: {props.resume.first_name}</p>
+      <p>Last_name: {props.resume.last_name}</p>
+      <p>Email: {props.resume.email}</p>
+      <p>Phone_number: {props.resume.phone_number}</p>
+      <p>Short_bio: {props.resume.short_bio}</p>
+      <p>Linkedin_url: {props.resume.linkedin_url}</p>
+      <p>Twitter_handle: {props.resume.twitter_handle}</p>
+      <p>Personal_website_url: {props.resume.personal_website_url}</p>
+      <p>Online_resume_url: {props.resume.online_resume_url}</p>
+      <p>Github_url: {props.resume.github_url}</p>
     </div>
   );
 }
